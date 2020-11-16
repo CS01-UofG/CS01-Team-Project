a2cf8b0a734fa1b5e1e84a45fd17754552f9790b
Krasimir Ivanov <krasimir2.ivanov@abv.bg>
2020-11-16 13:52:23 +0200

    Fixed point label not shown because fov is above the point [issue#13]

--- ElectronJSApp/main-viewer.js
+++ ElectronJSApp/main-viewer.js
@@ -5,7 +5,7 @@ var viewer = new Cesium.Viewer("cesiumContainer", {
 // in the future this will be received from the sensor 
 // currently hard coded
 // Glasgow Uni 55.873543, 	-4.289058
-var sensorLong =  -4.289058;
+var sensorLong = -4.289058;
 var sensorLat = 55.873543;
 
 // Show user Long and lat 
@@ -15,17 +15,17 @@ document.getElementById("latitude").innerHTML = sensorLat;
 viewer.scene.camera.setView({
     destination: Cesium.Cartesian3.fromDegrees(sensorLong, sensorLat, 40000000.0),
     orientation: {
-      heading: Cesium.Math.toRadians(0),
+        heading: Cesium.Math.toRadians(0),
     },
-  });
+});
 // this is 1 meter in degrees
 const coef = 0.0000089;
 
 // distance between object of interest and the sensor in meters
-var objectDistanceFromSensor = 150;
-var sensorAngleEquator = 280; // in degrees
+var objectDistanceFromSensor = 140.0;
+var sensorAngleEquator = 270.0; // in degrees
 // not sure about the name, this is just how wide the field of view is
-var sensorVisionAngle = 30;
+var sensorVisionAngle = 30.0;
 var sensorMaxVisibility = 160.0;
 
 // converting sensor from degrees to cartesian coordinates i.e. (x, y, z)
@@ -47,12 +47,12 @@ var user = viewer.entities.add({
 });
 
 // distance from sensor to object in x and y
-deltaX = Math.sin(sensorAngleEquator * Math.PI / 180) * objectDistanceFromSensor;
-deltaY = Math.cos(sensorAngleEquator * Math.PI / 180) * objectDistanceFromSensor;
+deltaY = Math.sin(sensorAngleEquator * Math.PI / 180.0) * objectDistanceFromSensor;
+deltaX = Math.cos(sensorAngleEquator * Math.PI / 180.0) * objectDistanceFromSensor;
 
 // https://stackoverflow.com/questions/7477003/calculating-new-longitude-latitude-from-old-n-meters
-var objectLat = sensorLat + coef * deltaX;
-var objectLong = sensorLong + (coef * deltaY) / Math.cos(sensorLat * 0.018)
+var objectLat = sensorLat + coef * deltaY;
+var objectLong = sensorLong + (coef * deltaX) / Math.cos(sensorLat * Math.PI / 180.0)
 
 var newCartesian = Cesium.Cartesian3.fromDegrees(objectLong, objectLat);
 
@@ -63,7 +63,7 @@ viewer.entities.add({
         pixelSize: 5,
         color: Cesium.Color.RED,
         outlineColor: Cesium.Color.WHITE,
-        outlineWidth: 2
+        outlineWidth: 2,
     },
     description: `Distance from sensor: ${objectDistanceFromSensor} m. <br/> Lat: ${objectLat} <br/> Long: ${objectLong}`,
 });
@@ -74,10 +74,10 @@ var fov = viewer.entities.add({
     position: sensorCartesian,
     ellipsoid: {
         radii: new Cesium.Cartesian3(sensorMaxVisibility, sensorMaxVisibility, sensorMaxVisibility),
-        innerRadii: new Cesium.Cartesian3(1, 1, 1),
-        minimumClock: Cesium.Math.toRadians(sensorAngleEquator - sensorVisionAngle / 2),
-        maximumClock: Cesium.Math.toRadians(sensorAngleEquator + sensorVisionAngle / 2),
-        minimumCone: Cesium.Math.toRadians(89.8),
+        innerRadii: new Cesium.Cartesian3(0.1, 0.1, 0.1),
+        minimumClock: Cesium.Math.toRadians(sensorAngleEquator - (sensorVisionAngle / 2.0)),
+        maximumClock: Cesium.Math.toRadians(sensorAngleEquator + (sensorVisionAngle / 2.0)),
+        minimumCone: Cesium.Math.toRadians(90.19),
         maximumCone: Cesium.Math.toRadians(90.2),
         material: new Cesium.Color.fromCssColorString('yellow').withAlpha(0.5),
         outline: true,
