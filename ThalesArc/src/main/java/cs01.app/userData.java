b99914c01db4c2b8ed79946bfacc7ee1383c79f0
Krishang Patney <krishangpatney@icloud.com>
2021-02-21 10:51:03 +0000

    Added JavaMaven project with project assets already added in

---
+++ ThalesArc/src/main/java/cs01.app/userData.java
@@ -0,0 +1,7 @@
+package cs01.app;
+
+public class userData {
+    public static String host="localhost";
+    public static int port=8001;
+    public static String APIKey = "AAPKb67d305991d24fa89d69e310a58fa1f8xryw7mKYrKOZF9_A49GmTH_Bqj9GSEiAxI9Rv6Uqdj3ProCQ-S1D1dpYNNhW4mjk";
+}
